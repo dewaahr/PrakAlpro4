ffaaf65b0068cf76e5cfaec86d214ee2f3ad51e9
darkoboy <118077486+dewaahr@users.noreply.github.com>
2023-03-02 13:44:48 +0700

    Update haribulangeming.py

--- haribulangeming.py
+++ haribulangeming.py
@@ -1,4 +1,5 @@
 def jumlah_hari(nama_bulan):
+    #freestyle
     bulan_31=['januari','maret','mei','juli','agustus','oktober','desember']
     bulan_28=['februari']
     bulan_30=['april','juni','september','november']
@@ -9,6 +10,13 @@ def jumlah_hari(nama_bulan):
     elif nama_bulan in bulan_30:
         print('30 Hari')
     else:print('Tidak Valid')
+        
+        #nonfreestyle
+#     if nama_bulan == 'januari' or 'maret' or 'mei' or 'juli' or 'agustus' or 'oktober' or 'desember'
+#         print('31 Hari')
+#     elif nama_bulan == 'februari'
+#         print('28 Hari')
+#     elif nama_bulan == 'april' or 'juni' or 'september' or 'november'
 
 
 nama_bulan=input('Masukan Nama Bulan:')
